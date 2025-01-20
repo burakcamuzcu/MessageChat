ecb1d99918255b13f89ace03886802010ee33b69
burak camuzcu <burak.camuzcu@hotmail.com>
2025-01-20 14:50:29 +0300

    Initial Commit

---
+++ MessageChatTests/MessageChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  MessageChatTests.swift
+//  MessageChatTests
+//
+//  Created by Burak Camuzcu on 20.01.2025.
+//
+
+import Testing
+@testable import MessageChat
+
+struct MessageChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
